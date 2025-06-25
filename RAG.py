459e57e6930566f763984a5cb62c69860cb15b53
Zeyn <81753539+ZeynXu@users.noreply.github.com>
2025-06-26 01:01:54 +0800

    Add files via upload

--- RAG.py
+++ RAG.py
@@ -1,23 +1,31 @@
-from langchain_ollama import OllamaLLM,OllamaEmbeddings
+from langchain_ollama import OllamaLLM, OllamaEmbeddings
 from langchain_core import document_loaders
-from langchain_community.document_loaders import PyPDFLoader
-from langchain
+from langchain_docling import DoclingLoader
+from langchain_milvus import Milvus
 import langchain_text_splitters
 import langchain_chroma
 
 # 调用LLM
 model = OllamaLLM(model='deepseek-r1:7b')
-embeddings=OllamaEmbeddings(model='nomic-embed-text:latest')
+embeddings = OllamaEmbeddings(model='nomic-embed-text:latest')
 
 # 加载文档，切分文档
-loader=PyPDFLoader('')
+loader = DoclingLoader(file_path='')
+docs = loader.load()
 
 splitter = langchain_text_splitters.TextSplitter()
 docs = document_loaders.BaseLoader.load_and_split('', splitter)
 
 # 构造向量数据库和检索器
+URI = './milvus_vs.db'
+vector_store = Milvus.from_documents(
+    docs, embeddings,
+    collection_name='',
+    connection_args={'uri': URI},
+)
 
-
+retriever=vector_store.as_retriever()
+retriever.invoke('')
 
 # 检索结果重排序
 
